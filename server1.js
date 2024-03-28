0c52cefdd6cdd253c64f72ec4737fd6c7498d905
Benjirez <benjirez@gmail.com>
2024-03-28 10:32:21 -0700

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
-const connectDB = require('./db') //function to connect with db
+const mongoose = require('mongoose')
+
+//const connectDB = require('./db') //function to connect with db
 const express = require('express')
 const app = express()
 const cors = require('cors');
@@ -7,6 +9,7 @@ const cors = require('cors');
 const oldRouter = require('./routes/oldCollections.js')
 const PORT = process.env.PORT || 3000
 
+/*
 app.use(cors());
 app.use(express.json())
 
@@ -19,7 +22,34 @@ app.use('/old', oldRouter )
     await connectDB()
     app.listen(PORT, ()=>{ console.log('server1 in effect...') }) 
 })()
+*/
 
+/* ################################################################## */
 
+//const express = require('express')
 
+const app = express()
+const PORT = process.env.PORT || 3000
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_URI);
+    console.log(`MC DB ona mic: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+//Routes go here
+app.use(cors());
+app.use(express.json())
+
+app.use('/old', oldRouter )
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("yeeee boyeee listening");
+    })
+})
