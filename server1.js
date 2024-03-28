4b44817eacf129973bf6a6170b017b71928e6806
Benjirez <benjirez@gmail.com>
2024-03-28 10:41:12 -0700

    Update server1.js cleanup

--- server1.js
+++ server1.js
@@ -1,35 +1,11 @@
 // require('dotenv').config()
 const mongoose = require('mongoose')
-
-//const connectDB = require('./db') //function to connect with db
 const express = require('express')
 const app = express()
 const cors = require('cors');
-//const dataRouter = require('./routes/data.js')
 const oldRouter = require('./routes/oldCollections.js')
 const PORT = process.env.PORT || 3000
 
-/*
-app.use(cors());
-app.use(express.json())
-
-app.use('/old', oldRouter )
-//app.use('/data', dataRouter)
-
-
-
-;( async ()=>{
-    await connectDB()
-    app.listen(PORT, ()=>{ console.log('server1 in effect...') }) 
-})()
-*/
-
-/* ################################################################## */
-
-//const express = require('express')
-
-//const app = express()
-//const PORT = process.env.PORT || 3000
 
 const connectDB = async () => {
   try {
@@ -42,9 +18,9 @@ const connectDB = async () => {
 }
 
 //Routes go here
+
 app.use(cors());
 app.use(express.json())
-
 app.use('/old', oldRouter )
 
 //Connect to the database before listening
