b0cc1de294d2c67e784dfbd5aff9ac6b10db24c6
Benjirez <benjirez@gmail.com>
2024-05-23 14:10:48 -0700

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,11 +1,9 @@
 // require('dotenv').config()
-const mongoose = require('mongoose')
-const express = require('express')
-const app = express()
-//const cors = require('cors');
-const oldRouter = require('./routes/oldCollections.js')
-const PORT = process.env.PORT || 3000
-
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const oldRouter = require('./routes/oldCollections.js');
+const PORT = process.env.PORT || 3000;
 
 const connectDB = async () => {
   try {
@@ -15,26 +13,12 @@ const connectDB = async () => {
     console.log(error);
     process.exit(1);
   }
-}
-
-//Routes go here
-
-/*
-// Configure CORS
-const corsOptions = {
-  origin: 'https://rez-client-01.vercel.app/', // Replace with the client origin
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-// Enable CORS for all routes
-app.use(cors(corsOptions));
+// Parse JSON request bodies
+app.use(express.json());
 
-
-*/
-
-app.use(express.json())
-
-// Routes
+// CORS middleware
 app.use('/old', (req, res, next) => {
   // Set CORS headers
   res.setHeader('Access-Control-Allow-Origin', 'https://rez-client-01.vercel.app');
@@ -47,14 +31,14 @@ app.use('/old', (req, res, next) => {
   } else {
     next();
   }
-}, oldRouter);
+});
 
-//app.use(cors());
-// app.use('/old', oldRouter )
+// Routes
+app.use('/old', oldRouter);
 
-//Connect to the database before listening
+// Connect to the database before listening
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("yeeee boyeee listening");
-    })
-})
+  app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+  });
+});
