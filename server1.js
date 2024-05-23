f4d5c8be35b48328fb95116a03589c675a0430ef
Benjirez <benjirez@gmail.com>
2024-05-23 12:58:23 -0700

    Update server1.js

--- server1.js
+++ server1.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
-const cors = require('cors');
+// const cors = require('cors');
 const oldRouter = require('./routes/oldCollections.js')
 const PORT = process.env.PORT || 3000
 
@@ -19,6 +19,7 @@ const connectDB = async () => {
 
 //Routes go here
 
+/*
 // Configure CORS
 const corsOptions = {
   origin: 'https://rez-client-01.vercel.app/', // Replace with the client origin
@@ -29,9 +30,26 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 //app.use(cors());
+*/
 
 app.use(express.json())
-app.use('/old', oldRouter )
+
+// Routes
+app.use('/old', (req, res, next) => {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', 'https://rez-client-01.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+}, oldRouter);
+
+// app.use('/old', oldRouter )
 
 //Connect to the database before listening
 connectDB().then(() => {
