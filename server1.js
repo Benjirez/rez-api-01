c2726e8828f362c91978eb31845302ed04a51508
Benjirez <benjirez@gmail.com>
2024-05-24 09:41:52 -0700

    Update server1.js

--- server1.js
+++ server1.js
@@ -1,48 +1,46 @@
 // require('dotenv').config()
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-const oldRouter = require('./routes/oldCollections.js');
-const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose')
+
+//const connectDB = require('./db') //function to connect with db
+const express = require('express')
+const app = express()
+const cors = require('cors');
+//const dataRouter = require('./routes/data.js')
+const oldRouter = require('./routes/oldCollections.js')
+const PORT = process.env.PORT || 3000
+
+/*
+app.use(cors());
+app.use(express.json())
+app.use('/old', oldRouter )
+//app.use('/data', dataRouter)
+;( async ()=>{
+    await connectDB()
+    app.listen(PORT, ()=>{ console.log('server1 in effect...') }) 
+})()
+*/
+
+/* ################################################################## */
+
+//const express = require('express')
+
+//const app = express()
+//const PORT = process.env.PORT || 3000
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DB_URI);
-    console.log(`MC DB ona mic: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-};
+	@@ -42,9 +18,9 @@ const connectDB = async () => {
+}
 
-// CORS middleware
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', 'https://rez-client-01.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+//Routes go here
+app.use(cors());
+app.use(express.json())
 
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
+app.use('/old', oldRouter )
 
-  return await fn(req, res);
-};
-
-// Parse JSON request bodies
-app.use(express.json());
-
-// Routes
-//app.use('/old', allowCors(oldRouter));
-app.use('/old', oldRouter);
-
-// Connect to the database before listening
+//Connect to the database before listening
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
-  });
-});
+    app.listen(PORT, () => {
+        console.log("yeeee boyeee listening");
+    })
+})
