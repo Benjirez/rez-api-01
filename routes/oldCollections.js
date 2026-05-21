c419a2be02f4b9fe56560a75c6a9b58c263d350d
Benjirez <benjirez@gmail.com>
2026-05-21 15:02:16 -0700

    Refactor oldCollections.js to use dynamic models

--- routes/oldCollections.js
+++ routes/oldCollections.js
@@ -1,77 +1,75 @@
-const express = require('express')
-const router = express.Router()
-const oldCols= require('../models/OldCollections.js')
-//get all
-let collPick = 0
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const oldCols = require('../models/OldCollections.js');
+const StoreName = require('../models/StoreName.js');
 
+const UserSchema = new mongoose.Schema({
+  col_a: String, col_b: String, col_c: String, col_d: String,
+  col_e: String, col_f: String, col_g: String, col_h: String,
+  col_i: String, col_j: String,
+});
 
+let collPick = 0;
 
-router.get( '/:pick', async (req, res)=>{ 
-    collPick = req.params.pick
-    try{
-        //console.log(oldCols[0])
-        const myData = await oldCols[req.params.pick].find()
-        //const myData = await Data.find()
-        console.log ('my data is: ' + myData.length)
-        res.json( myData )
-    }catch(err){
-        console.log('nope')
-        res.status(500).json( {message: err.message}) //server or DB error (not user)
-    }
+const getModel = async (pick) => {
+  const stores = await StoreName.find().sort({ _id: 1 });
+  if (stores[pick]) {
+    const collName = stores[pick].collectionName;
+    return mongoose.models[collName] ||
+           mongoose.model(collName, UserSchema, collName);
+  }
+  return oldCols[pick];
+};
 
-} )
+router.get('/:pick', async (req, res) => {
+  collPick = req.params.pick;
+  try {
+    const Model = await getModel(parseInt(collPick));
+    const myData = await Model.find();
+    res.json(myData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-//get one 
-router.get( '/:id', async (req, res)=>{ 
+router.post('/', async (req, res) => {
+  try {
+    const Model = await getModel(parseInt(collPick));
+    const data = new Model({
+      col_a: req.body.col_a, col_b: req.body.col_b, col_c: req.body.col_c,
+      col_d: req.body.col_d, col_e: req.body.col_e, col_f: req.body.col_f,
+      col_g: req.body.col_g, col_h: req.body.col_h
+    });
+    await data.save();
+    const myData = await Model.find();
+    res.json(myData);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
-    try{
-        const data = await Data.findById()
-        res.json( data )
-    }catch(err){
-        console.log('nope')
-        res.status(500).json( {message: err.message}) //server or DB error (not user)
-    }
-    
-} )
+router.patch('/:id', async (req, res) => {
+  try {
+    const Model = await getModel(parseInt(collPick));
+    const data = await Model.findById(req.params.id);
+    if (req.body.title != null) { data.title = req.body.title; }
+    await data.save();
+    res.send(req.params.id);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-//create one
-router.post( '/', async (req, res)=>{ 
-    const data = new oldCols[collPick]({
-        col_a: req.body.col_a, 
-        col_b: req.body.col_b, 
-        col_c: req.body.col_c, 
-        col_d: req.body.col_d, 
-        col_e: req.body.col_e, 
-        col_f: req.body.col_f, 
-        col_g: req.body.col_g, 
-        col_h: req.body.col_h
-      })
-    try{
-        await data.save()
-        const myData = await oldCols[collPick].find()
-        //console.log ('my data is: ' + myData.length)
-        res.json( myData )
-    }catch(err){
-        res.status(400).json( {message: err.message}) // 400 bad user data
-        
-    }
-} )
+router.delete('/:id', async (req, res) => {
+  try {
+    const Model = await getModel(parseInt(collPick));
+    await Model.findByIdAndDelete(req.params.id);
+    const myData = await Model.find();
+    res.json(myData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
-//patch one
-router.patch( '/:id', async (req, res)=>{ 
-
-    const data = await oldCols[collPick].findById( req.params.id )
-   if (req.body.title != null){ data.title = req.body.title }
-    data.save()
-    res.send( req.params.id )
-} )
-
-//delete one
-router.delete( '/:id', async (req, res)=>{ 
-    await oldCols[collPick].findByIdAndDelete( req.params.id ); 
-    const myData = await oldCols[collPick].find()
-        //console.log ('my data is: ' + myData.length)
-        res.json( myData )
-} )
-
-module.exports = router
+module.exports = router;
