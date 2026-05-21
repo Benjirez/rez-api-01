7df28cf645a046c76f7a3553820b66768b2ae810
Benjirez <benjirez@gmail.com>
2026-05-21 15:42:12 -0700

    Make collectionName field unique in StoreName schema

--- models/StoreName.js
+++ models/StoreName.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const StoreNameSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  collectionName: { type: String, required: true }
+  collectionName: { type: String, required: true, unique: true }
 });
 
 module.exports = mongoose.model('StoreName', StoreNameSchema, 'store_names');
