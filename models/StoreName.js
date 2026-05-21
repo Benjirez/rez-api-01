c9fef51daee0afc02156295086a3bd0b3f96f1bb
Benjirez <benjirez@gmail.com>
2026-05-21 15:00:29 -0700

    Add collectionName field to StoreName schema

--- models/StoreName.js
+++ models/StoreName.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const StoreNameSchema = new mongoose.Schema({
-  name: { type: String, required: true }
+  name: { type: String, required: true },
+  collectionName: { type: String, required: true }
 });
 
 module.exports = mongoose.model('StoreName', StoreNameSchema, 'store_names');
